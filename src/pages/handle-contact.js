dc583ab82a4e645f5127678014e0d2cbb1b75d31
barab <94920370+Baraban511@users.noreply.github.com>
2024-08-26 16:03:38 +0200

    Finish contact and start blog

--- src/pages/handle-contact.js
+++ src/pages/handle-contact.js
@@ -1,35 +1,51 @@
+import { Client, EmbedBuilder, Events, GatewayIntentBits, } from "discord.js";
+const token = import.meta.env.DISCORD_TOKEN;
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, () => {
+    console.log(`Sending message...`);
+});
+
 export async function POST({ request }) {
     try {
         const data = await request.formData();
         const email = data.get("email");
         const message = data.get("message");
-        console.log(email, message);
-
-        if (!email || !message) {
-            return new Response(
-                JSON.stringify({
-                    message: "Missing required fields",
-                }),
-                { status: 400 }
+        if (!message) {
+            const headers = new Headers();
+            headers.set("location", "/contact");
+            return new Response(null, {
+                status: 303, // See Other
+                headers,
+            });
+        }
+        const contactEmbed = new EmbedBuilder()
+            .setColor("#008080")
+            .setTitle("Contact request on barab.me")
+            .setAuthor({
+                name: "barab",
+                iconURL: "https://barab.me/barab_logo_fox.png",
+                url: "https://barab.me",
+            })
+            .addFields(
+                { name: "Message", value: `${message} \n` },
+                { name: "\u200B", value: "\u200B" },)
+            .setTimestamp()
+            .setFooter({
+                text: request.url,
+            });
+        if (email) {
+            contactEmbed.addFields(
+                {
+                    name: "Mail",
+                    value:
+                        `${email} \n \n [**Répondre**](https://barab.me/mail/${email}) \n`,
+                },
             );
         }
+        client.login(token);
+		const user = await client.users.fetch(import.meta.env.USER_ID);
+        user.send({ embeds: [contactEmbed] });
 
-        await fetch('https://api.useplunk.com/v1/send', {
-
-            method: 'POST',
-
-            body: JSON.stringify({
-                to: import.meta.env.EMAIL_TO,
-                subject: "Contact on barab.me",
-                body: "Hey this is contact",
-                reply: "<string>",
-            }),
-
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${import.meta.env.PLUNK_KEY}`,
-            },
-        });
         const headers = new Headers();
         headers.set("location", "/thanks-for-contact");
         return new Response(null, {
