cd9e70eff4ed160de7e6fa2555def53f04874b92
barab <94920370+Baraban511@users.noreply.github.com>
2024-09-10 17:59:30 +0200

    Add sitemap and build day

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-
+import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
-
+const website = 'https://barab.me';
 // https://astro.build/config
 export default defineConfig({
+  site: website,
   prefetch: true,
   output: "server",
   adapter: cloudflare({
@@ -18,11 +19,18 @@ export default defineConfig({
     build: {
       minify: false,
     },
+    define: {
+      'import.meta.env.BUILD_DATE': JSON.stringify(new Date().toISOString().split('T')[0]),
+    },
   },
   integrations: [tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     applyBaseStyles: false,
+  }),
+  sitemap({
+    filter: (page) => page !== `${website}/thanks-for-contact/`,
+    lastmod: new Date(Date.now()),
   }),
   icon()]
 });
---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from 'astro';
+
+const getRobotsTxt = (sitemapURL: URL) => `
+User-agent: *
+Disallow: /thanks-for-contact
+
+Sitemap: ${sitemapURL.href}
+`;
+
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL('sitemap-index.xml', site);
+  return new Response(getRobotsTxt(sitemapURL));
+};
