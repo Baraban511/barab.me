ee7946a7324a1c979f5e781fa6bfd6f58e405c07
barab <94920370+Baraban511@users.noreply.github.com>
2024-10-02 14:13:43 +0200

    Update astro

---
+++ src/pages/presence/activities/vscode.js
@@ -0,0 +1,13 @@
+export async function GET({ request }) {
+    const userId = import.meta.env.USER_ID;
+    var presence = await fetch("https://api.statusbadges.me/presence/" + userId);
+    presence = await presence.json();
+    var activities = presence.activities;
+    console.log(activities);
+    for (var i = 0; i < activities.length; i++) {
+        if (activities[i].name == "Visual Studio Code") {
+            return new Response(JSON.stringify(activities[i]))
+        }
+    }
+    return new Response(false)
+}
