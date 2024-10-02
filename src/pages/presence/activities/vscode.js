bf58955d3f0e2320438cb9bd8982995e1d7780d1
barab <94920370+Baraban511@users.noreply.github.com>
2024-10-02 18:32:20 +0200

    Clean console.log

--- src/pages/presence/activities/vscode.js
+++ src/pages/presence/activities/vscode.js
@@ -3,7 +3,6 @@ export async function GET({ request }) {
     var presence = await fetch("https://api.statusbadges.me/presence/" + userId);
     presence = await presence.json();
     var activities = presence.activities;
-    console.log(activities);
     for (var i = 0; i < activities.length; i++) {
         if (activities[i].name == "Visual Studio Code") {
             return new Response(JSON.stringify(activities[i]))
