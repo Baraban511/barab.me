dde04792b835bf9467e7d4b33e62673292d34de6
barab <94920370+Baraban511@users.noreply.github.com>
2024-10-04 07:16:00 +0000

    Fix secrets error

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
     },
     define: {
       'import.meta.env.BUILD_DATE': JSON.stringify(new Date().toISOString().split('T')[0]),
+      "process.env": process.env,
     },
   },
   integrations: [tailwind({
