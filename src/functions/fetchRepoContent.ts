00816238c77c3ae4233c33765edc742a70d3a04e
barab <94920370+Baraban511@users.noreply.github.com>
2024-12-30 13:01:45 +0100

    Switch to bun, start nsi page and minor changes

---
+++ src/functions/fetchRepoContent.ts
@@ -0,0 +1,27 @@
+export default async function fetchRepoContent(owner: string, repo: string, path: string, token: string) {
+    const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': `token ${token}`
+        }
+    });
+    const data: any[] = await response.json();
+    var files = []
+    for (const item of data) {
+        if (item.type === 'dir') {
+            const subFiles = await fetchRepoContent(owner, repo, item.path, token);
+            files = files.concat(subFiles);
+        } else {
+            if (item.name.endsWith('.py')) {
+                const response = await fetch(item.download_url, {
+                    headers: {
+                        'Authorization': `token ${token}`
+                    }
+                });
+                const file = await response.text()
+                files.push({ name: item.path, content: file })
+            }
+        }
+    }
+    return files
+}
