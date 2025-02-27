1e016768d96897ce4e29ad81c163623233faa241
barab. <Baraban511@users.noreply.github.com>
2025-02-27 22:27:05 +0100

    Starting migration of tailwind + update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
-import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-const website = 'https://barab.me';
+const website = "https://barab.me";
 // https://astro.build/config
 export default defineConfig({
   site: website,
@@ -11,27 +11,27 @@ export default defineConfig({
   output: "server",
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
+      enabled: true,
     },
-    imageService: 'passthrough',
+    imageService: "passthrough",
   }),
   vite: {
+    plugins: [tailwindcss()],
     build: {
       minify: false,
     },
     define: {
-      'import.meta.env.BUILD_DATE': JSON.stringify(new Date().toISOString().split('T')[0]),
+      "import.meta.env.BUILD_DATE": JSON.stringify(
+        new Date().toISOString().split("T")[0]
+      ),
       "process.env": process.env,
     },
   },
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false,
-  }),
-  sitemap({
-    filter: (page) => page !== `${website}/thanks-for-contact/`,
-    lastmod: new Date(Date.now()),
-  }),
-  icon()]
+  integrations: [
+    sitemap({
+      filter: (page) => page !== `${website}/thanks-for-contact/`,
+      lastmod: new Date(Date.now()),
+    }),
+    icon(),
+  ],
 });
