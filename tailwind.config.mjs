7188220b6e0214212cb71854a2c4dc59a80706d2
barab. <Baraban511@users.noreply.github.com>
2025-02-28 10:03:09 +0100

    Remove tailwind icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,14 @@
-const { addDynamicIconSelectors } = require('@iconify/tailwind');
-const tailwindForms = require('@tailwindcss/forms');
-import defaultTheme from 'tailwindcss/defaultTheme'
+const tailwindForms = require("@tailwindcss/forms");
+import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-		  fontFamily: {
-			sans: ['Noto Sans Variable', ...defaultTheme.fontFamily.sans],
-		  },
-		},
-	  },
-	plugins: [
-		addDynamicIconSelectors(),
-		tailwindForms,
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [tailwindForms],
+};
