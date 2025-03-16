0449df4bb7c31688f692db86d769869ce159657f
barab. <Baraban511@users.noreply.github.com>
2025-03-16 15:24:39 +0100

    Change to use fetch caching

--- src/functions/fetchRepoContent.ts
+++ src/functions/fetchRepoContent.ts
@@ -1,14 +1,23 @@
+const isCloudflareWorker = typeof caches !== "undefined";
+
 export default async function fetchRepoContent(
   owner: string,
   repo: string,
   path: string,
   token: string
 ) {
-  const cache = caches.default;
-  const cacheKey = new Request(`https://github.com/${owner}/${repo}/${path}`);
-  let cachedResponse = await cache.match(cacheKey);
+  const cacheKey = `https://github.com/${owner}/${repo}/${path}`;
 
-  if (cachedResponse) {
+  const cacheOptions = {
+    cf: { cacheEverything: true, cacheTtl: 86400 }, // Cache pour 24 heures
+  };
+
+  const cachedResponse = await fetch(cacheKey, {
+    ...cacheOptions,
+    method: "GET",
+  });
+
+  if (cachedResponse && cachedResponse.status === 200) {
     return await cachedResponse.json();
   }
 
@@ -26,41 +35,17 @@ export default async function fetchRepoContent(
     if (item.type === "dir") {
       const subFiles = await fetchRepoContent(owner, repo, item.path, token);
       files = files.concat(subFiles);
-    } else {
-      if (item.name.endsWith(".py")) {
-        const fileCacheKey = new Request(
-          `https://github.com/${owner}/${repo}/${item.path}`
-        );
-        let cachedFile = await cache.match(fileCacheKey);
-
-        if (cachedFile) {
-          console.log(`✅ Cache hit for file: ${item.path}`);
-          const fileContent = await cachedFile.text();
-          files.push({ name: item.path, content: fileContent });
-        } else {
-          console.log(`❌ Cache miss for file: ${item.path}`);
-          const response = await fetch(item.download_url, {
-            headers: {
-              Authorization: `token ${token}`,
-            },
-          });
-          const file = await response.text();
-          files.push({ name: item.path, content: file });
-
-          const fileResponse = new Response(file, {
-            headers: { "Content-Type": "text/plain" },
-          });
-
-          await cache.put(fileCacheKey, fileResponse.clone());
-        }
-      }
+    } else if (item.name.endsWith(".py")) {
+      const fileResponse = await fetch(item.download_url, {
+        ...cacheOptions,
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      });
+      const file = await fileResponse.text();
+      files.push({ name: item.path, content: file });
     }
   }
 
-  const jsonResponse = new Response(JSON.stringify(files), {
-    headers: { "Content-Type": "application/json" },
-  });
-
-  await cache.put(cacheKey, jsonResponse.clone());
   return files;
 }
