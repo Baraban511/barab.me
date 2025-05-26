2beac38ca1e51b8f0b943bf755e1ebc78efc2fa2
barab. <>
2025-05-26 21:53:58 +0200

    Change image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
     platformProxy: {
       enabled: true,
     },
-    imageService: "passthrough",
+    imageService: "cloudflare",
   }),
   vite: {
     plugins: [tailwindcss()],
